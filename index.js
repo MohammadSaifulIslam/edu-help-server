c4b7483c342c240d4dbcc71bd84aab276d8a48a4
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-07-23 21:14:08 -0700

    get all colleges route added

--- index.js
+++ index.js
@@ -25,6 +25,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const collegeCoection = client.db("eduHelpDb").collection("colleges");
+
+    // get all college
+    app.get("/colleges", async (req, res) => {
+      const result = await collegeCoection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
